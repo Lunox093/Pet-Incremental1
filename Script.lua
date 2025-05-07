10f1cd8f583e1f05d76faf90873962f8a09e1fdf
Lunox093 <120286771+Lunox093@users.noreply.github.com>
2025-05-07 19:33:45 +0300

    Big update Script.lua

--- Script.lua
+++ Script.lua
@@ -11,18 +11,18 @@ local Window = Rayfield:CreateWindow({
     },
     Discord = {
        Enabled = true,
-       Invite = "https://discord.gg/gbVWxRFtqt", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+       Invite = "gbVWxRFtqt", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
        RememberJoins = true -- Set this to false to make them join the discord every time they load it up
     },
     KeySystem = true, -- Set this to true to use our key system
     KeySettings = {
-       Title = "Pet Incremental | Key",
+       Title = "Pet Incremental",
        Subtitle = "Link In Discord Server",
-       Note = " join https://discord.gg/gbVWxRFtqt",
+       Note = "https://discord.gg/gbVWxRFtqt",
        FileName = "menyanetyhubkey", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+       SaveKey = false, -- The user's key will be saved, but if you change the key, they will be unable to use your script
        GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-       Key = {"https://pastebin.com/raw/3KjRX86z"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+       Key = {"https://pastebin.com/raw/kudCasSy"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
     }
  })
 
